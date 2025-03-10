57c3e50c9d9376f95e2969f96e5fae36388cc6b9
nedaras <nedas.pranskunas@gmail.com>
2025-03-10 14:04:58 +0200

    We're doing svgs in most idiotic way, but i think this is how we will do it on pngs.

--- src/main.lua
+++ src/main.lua
@@ -1,16 +1,63 @@
----@param str string
-local function snip(str)
-  local w = 9;
-  local h = 20;
-  
+---@param utf8_str string
+local function snip(utf8_str)
+  local max_w = 0;
+
+  local w = 10
+  local h = 20
+
+  local x = 0
+  local y = 15
+
+  local out = ""
+  -- svgs can have maximum of one space char
+
+  for _, rune in utf8.codes(utf8_str) do
+    local char = utf8.char(rune)
+    assert(char ~= '\t') -- ye
+
+    if char == ' ' then
+      x = x + w
+    elseif char == '\n' then
+      max_w = math.max(max_w, x)
+
+      x = 0
+      y = y + h
+    else
+      local c = char
+      if char == "<" then
+        c = "&lt;"
+      end
+
+      if char == ">" then
+        c = "&gt;"
+      end
+
+      if char == "&" then
+        c = "&amp;"
+      end
+
+      out = out .. "<text x=\"" .. x .. "\" y=\"" .. y .. "\" class=\"monospace\">" .. c .. "</text>\n"
+      x = x + w
+    end
+
+  end
+
+  --if #line ~= 0 then
+  --end
+
   print([[
-    <svg width="300" height="50" xmlns="http://www.w3.org/2000/svg">
-      <text x="10" y="40" font-family="Courier New, monospace" font-size="24" fill="red">]] .. str .. [[</text>
-    </svg>]])
+    <svg width="]] .. max_w .. [[" height="]] .. y ..[[" xmlns="http://www.w3.org/2000/svg">
+  <defs>
+    <style type="text/css">
+      <![CDATA[
+        .monospace {
+          font-family: 'Courier New', monospace;
+        }
+        ]] .. "]]" ..[[>
+    </style>
+  </defs>
+  ]] .. out .. "</svg>")
 end
 
-snip([[
----@param str string
-local function snip(str)
-  print(str)
-end]])
+local input = io.read("*a")
+snip(input)
